a465556fbbaae664c951c80eeb293b8cd52389a7
Eduardo Grando Lopes <eduardogl281207@gmail.com>
2026-04-30 20:40:45 -0300

    cobrinho tomando forma

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //aahh
 let grid = document.querySelector(".gameGrid");
+let pos = 50;
 const mapSize = 100;
 
 for(let i = 0; i < mapSize; i++){
@@ -8,3 +9,27 @@ for(let i = 0; i < mapSize; i++){
   cell.id = `cell${i}`;
   grid.appendChild(cell);
 }
+
+document.addEventListener("keydown", (event) => {
+  let sneki = document.querySelector(`#cell${pos}`);
+
+  switch(event.key){
+    case 'w':
+      pos -= 10;
+      break;
+    case 'a':
+      pos -= 1;
+      break;
+    case 's':
+      pos += 10;
+      break;
+    case 'd':
+      pos += 1;
+      break;
+    default:
+      break;
+  }
+
+  sneki = document.querySelector(`#cell${pos}`);
+  sneki.style.visibility = "visible";
+});
